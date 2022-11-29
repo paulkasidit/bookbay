dbe43dbb7543d2a2eb43f0268b653b56a68236c5
Paul Kasidit <paulkasidit@gmail.com>
2022-11-28 19:56:04 -0800

    delete placeholder text

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,3 @@ end
 p "Created #{Book.count} books"
 p "Created #{Review.count} reviews"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
