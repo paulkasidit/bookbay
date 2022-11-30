3e55eb4e5bc47176e2640f833782c609a451a205
Paul Kasidit <paulkasidit@gmail.com>
2022-11-30 02:29:54 -0800

    wrote cart controller methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,42 @@
 class CartsController < ApplicationController
 
   def index
-    @carts = User.find(params[:user_id])
+    @cart = User.find(params[:user_id])
     json_response(@reviews)
   end
 
-  
+  def create
+    @user = User.find(user_params)
+    @cart = @user.carts.new(cart_params)
+    json_response(@cart, :created) 
+  end
 
+  def show
+    @user = User.find(user_params)
+    @cart = Cart.find(params[:id])
+    json_response(@cart)
+  end
+
+  def update
+    @cart = Cart.find(params[:id])
+    if @cart.update!(cart_params)
+      render status: 200, json: {
+        message: "Cart has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @cart = Cart.find(params[:id])
+    if @cart.destroy!(cart_params)
+      render status: 200, json: {
+        message: "Cart has been deleted successfully."
+      }
+    end
+  end 
+
+  private
+  def cart_params
+    params.require(:cart).permit(:user_id, :book_id)
+  end 
 end
