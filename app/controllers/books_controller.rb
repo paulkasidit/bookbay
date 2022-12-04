0b7946ab0bff3debc903a9a6fe78cbc3f5077159
Paul Kasidit <paulkasidit@gmail.com>
2022-12-04 14:03:40 -0800

    complete listed by function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    user = User.find(@book.user_id)
+    @user_username = user.username
     render :show
   end
 
