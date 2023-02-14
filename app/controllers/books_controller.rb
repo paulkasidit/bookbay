05e4e3027643d4c3b420a8775654a2b7810ac8f7
Paul Kasidit <paulkasidit@gmail.com>
2023-02-13 18:43:45 -0800

    fixed bugs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     user = User.find(@book.user_id)
-    @user_username = user.username
+    @user_email = user.email
     render :show
   end
 
